784b08953be04dac317be19eab1b9cb8d7603e25
MaxShvachko <maxshvachko@ukr.net>
2023-06-11 19:56:03 +0300

    Add a preview image

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,31 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/drag-and-drop-example/'
+  base: '/drag-and-drop-example/',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (info) => {
+          // Keep CSS files with a hash in the filename
+          if (info.name?.endsWith('.css')) {
+            return 'assets/[name].[hash].css';
+          }
+          // Exclude JPEG and PNG files from dynamic file generation
+          if (
+            info.name?.endsWith('.jpg')
+          || info.name?.endsWith('.jpeg')
+          || info.name?.endsWith('.png')
+          || info.name?.endsWith('.ico')
+          ) {
+            return '[name].[ext]';
+          }
+          // Generate other files with a hash in the filename
+          return 'assets/[name].[hash].[ext]';
+        },
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  },
+  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.ico']
 });
